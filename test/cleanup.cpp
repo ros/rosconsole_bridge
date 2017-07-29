707a68c1b771024b64be9ffcc961855ae4fd4e9d
HannesSommer <hannes.sommer@mavt.ethz.ch>
2017-07-29 10:38:01 +0200

    Simplified the cleanup test program

--- test/cleanup.cpp
+++ test/cleanup.cpp
@@ -1,28 +1,11 @@
-#include <console_bridge/console.h>
 #include <rosconsole_bridge/bridge.h>
-#include <string>
-
-struct A {
-  A(const char* hint) :  hint_(hint) {
-    logWarn("initializing class: %s", hint);
-  }
-  ~A() {
-    if(hint_ == "static"){ // mimic original behavior
-      rosconsole_bridge::deactivate();
-    }
-    logWarn("destroying class: %s", hint_.c_str());
-  }
-private:
-  std::string hint_;
-};
-
-// destructor of static instance should use the original output handler
-static A a("static");
 
 REGISTER_ROSCONSOLE_BRIDGE;
 
 int main(int argc, char **argv)
 {
-  A a("local");
+  logWarn("This warning should be delivered through rosconsole");
+  rosconsole_bridge::deactivate();
+  logWarn("This warning should be delivered through the original console_bridge output handler");
   return 0;
 }
